3e94252d2e5e2d3219bc3a4a1997dd1cf2c61cbd
Iamneo <techadmin@iamneo.ai>
2023-08-07 05:36:32 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/TasksService.java
+++ springapp/src/main/java/com/examly/springapp/service/TasksService.java
@@ -1,3 +1,14 @@
+package com.examly.springapp.service;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Task;
+import com.examly.springapp.repository.TasksRepository;
+
+@Service
 public class TasksService {
-    
+    @Autowired
+    TasksRep
 }
