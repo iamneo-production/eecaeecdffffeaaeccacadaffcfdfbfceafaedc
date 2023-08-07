39970e16b5630b06f8025acf8695ae6da6fccaf5
Iamneo <techadmin@iamneo.ai>
2023-08-07 06:01:32 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.examly.springapp.controller;
 
 import java.util.List;
+
+import javax.annotation.processing.Generated;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.examly.springapp.model.Task;
-import com.examly.springapp..service.TaskSer
+import com.examly.springapp..service.TasksService;
 
+@RestController
 public class TaskController {
-    
+    @Autowired
+    TasksService tasksService;
+
+    @GetMapping("/alltasks")
+    public List<Task> getAllTasks(){
+        return tasksService.getAllTasks();
+    }
+
+    @GetMapping("/getTask/{taskId}")
+    public Task getTask(@PathVariable("taskId") String taskId){
+        return tasksService.getTaskById(taskId);
+    }
+
+    @DeleteMapping("/deleteTa")
 }
